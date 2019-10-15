da9c752258b586b6167202b14dc5959d759982bc
Connor Holly <connorholly11@gmail.com>
2019-10-15 12:54:44 -0400

    need to add logout

--- api/server.js
+++ api/server.js
@@ -1,21 +1,35 @@
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
 
-const authRouter = require('../auth/auth-router.js');
-const usersRouter = require('../users/users-router.js');
+const authRouter = require("../auth/auth-router.js");
+const usersRouter = require("../users/users-router.js");
+const session = require("express-session");
 
 const server = express();
+
+const sessionConfig = {
+  name: "cookie",
+  secret: "I am a secret!",
+  cookie: {
+    maxAge: 1000 * 10,
+    secure: false, //it should be true in production
+    httpOnly: true
+  },
+  resave: false,
+  saveUninitalized: false
+};
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
-server.use('/api/auth', authRouter);
-server.use('/api/users', usersRouter);
+server.use(session(sessionConfig));
+server.use("/api/auth", authRouter);
+server.use("/api/users", usersRouter);
 
-server.get('/', (req, res) => {
-  res.json({ api: 'up' });
+server.get("/", (req, res) => {
+  res.json({ api: "up" });
 });
 
 module.exports = server;
